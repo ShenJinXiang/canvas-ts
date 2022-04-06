97d5c5ae1ff0eeea246b09a4fdeefb39f67d6b2f
shenjinxiang <sjx-sword@163.com>
2022-04-06 21:15:04 +0800

    象棋棋盘边框

--- src/components/page/2020/ChineseChessBoard/index.ts
+++ src/components/page/2020/ChineseChessBoard/index.ts
@@ -12,6 +12,13 @@ interface IOption {
   outerWidth: number,
 }
 
+interface IRect {
+  oringX: number,
+  oringY: number,
+  width: number,
+  height: number,
+}
+
 export default class ChineseChessBoard {
   private canvas: HTMLCanvasElement | null = null;
   private context: CanvasRenderingContext2D | null = null;
@@ -22,6 +29,7 @@ export default class ChineseChessBoard {
   private option: IOption;
   private lines: Line[] = [];
   private corners: Corner[] = [];
+  private outerRect: IRect | null = null;
   constructor(gridWidth: number) {
     this.option = {
       background: '#fdf5db',
@@ -94,6 +102,13 @@ export default class ChineseChessBoard {
     this.corners.push(new Corner(8 * gridWidth, 6 * gridWidth, [1, 2], this.option.cornerDistance, this.option.cornerWidth));
     this.corners.push(new Corner(1 * gridWidth, 7 * gridWidth, [0, 1, 2, 3], this.option.cornerDistance, this.option.cornerWidth));
     this.corners.push(new Corner(7 * gridWidth, 7 * gridWidth, [0, 1, 2, 3], this.option.cornerDistance, this.option.cornerWidth));
+
+    this.outerRect = {
+      oringX: -this.option.margin,
+      oringY: -this.option.margin,
+      width: 2 * this.option.margin + 8 * this.option.gridWidth,
+      height: 2 * this.option.margin + 9 * this.option.gridWidth,
+    };
   }
 
   initCanvas(canvas: HTMLCanvasElement) {
@@ -115,13 +130,25 @@ export default class ChineseChessBoard {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.context.fillStyle = this.option.background;
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    this.context.translate(this.ox, this.ox);
+    this.context.translate(this.ox, this.oy);
     this.lines.forEach((item) => {
       item.draw(this.context, this.option.lineColor, 1);
     });
     this.corners.forEach((item) => {
       item.draw(this.context, this.option.lineColor, 1);
     });
+
+    if (this.outerRect) {
+      this.context.beginPath();
+      this.context.lineWidth = this.option.borderWidth;
+      this.context.strokeStyle = this.option.lineColor;
+      this.context.strokeRect(
+        this.outerRect.oringX,
+        this.outerRect.oringY,
+        this.outerRect.width,
+        this.outerRect.height
+      );
+    }
     this.context.restore();
   }
 }
